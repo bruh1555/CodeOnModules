0c065731c537bb6246df54c437767488499da3a5
bruh1555 <128431763+bruh1555@users.noreply.github.com>
2024-08-06 12:18:28 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+print("You installed this module the wrong way.")
+print("Please use the mdl command, then use option 1, then type in the module you'd like to install from here.")
+# note: this is just for warning the user they installed these modules the wrong way
